1ec26774763643f1397335b2e8bb02ea79137dd8
CJ <cmaceze@gmail.com>
2018-10-13 03:12:05 +0000

    change mlab

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes   = require("./routes/comments"),
 
 //declarations
 // mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });   //LOCAL DB
-mongoose.connect("mongodb://CJ:test@ds131313.mlab.com:31313/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://CJ:cjtest11@ds131313.mlab.com:31313/yelp_camp", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-// seedDB(); seed DB
+// seedDB(); seed DB for test purposes
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
