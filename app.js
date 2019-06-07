359b55ac18046b43ceb84d8174f5622fbf8f5b8f
Chijioke Mac Eze <cmaceze@gmail.com>
2019-06-07 12:05:05 +0000

    app.js change

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ let commentRoutes   = require("./routes/comments"),
     indexRoutes     = require("./routes/index");
 
 //declarations
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://CJ:webdevCJ11@ds063946.mlab.com:63946/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
