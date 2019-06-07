a6a260ca60d533fc291b49ce260720538011f3f8
Chijioke Mac Eze <cmaceze@gmail.com>
2019-06-07 15:14:48 +0000

    change db to live and env

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ let commentRoutes   = require("./routes/comments"),
 //declarations
 
 // !Live DB
-// mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 // ? local DB
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -63,6 +63,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 //SERVER
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Yelpcamp Server Has Started");
 });
